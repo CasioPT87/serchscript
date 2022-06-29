bf4087adb0dfc7bab927255e6f94b6c675d95fad
Sergio <sergioibanezmoreno@gmail.com>
2022-06-29 09:09:00 +0200

    WIP

--- api/routes/data/admin/index.js
+++ api/routes/data/admin/index.js
@@ -3,6 +3,7 @@ const { authorization } = require('../../middlewares')
 
 const adminArticles = require('./articles')
 const adminComments = require('./comments')
+const adminImages = require('./images')
 
 const router = express.Router()
 
@@ -10,6 +11,7 @@ const router = express.Router()
 
 router.use('/articles', adminArticles)
 router.use('/comments', adminComments)
+router.use('/images', adminImages)
 
 
 module.exports = router;
