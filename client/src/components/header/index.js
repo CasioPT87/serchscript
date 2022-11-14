a3f61e25c7a15f5ebc272277135ee601d12ce8e8
Zerch <sergioibanezmoreno@gmail.com>
2022-11-14 18:58:19 +0100

    Header access logo

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="header">
       <Link to="/">
-        <img src={"images/logo.png"} className="logo" alt="flamyduck logo" />
+        <img src={'images/logo.png'} className="logo" alt="flamyduck logo" />
       </Link>
       <Link to="/">Home</Link> | <Link to="/articles">Articles</Link> |{' '}
       <Link to="/admin/articles/new">new Article</Link> |{' '}
