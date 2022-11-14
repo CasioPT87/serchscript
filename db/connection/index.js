26bce6bc81d3c98ecace9cc7bf5588a2caea8d0a
Zerch <sergioibanezmoreno@gmail.com>
2022-11-14 16:19:37 +0100

    Connected to db again

--- db/connection/index.js
+++ db/connection/index.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 const { MONGODB_URI } = process.env
 
 const connect = () => {
-    const mongoDB = MONGODB_URI || "mongodb://jamona:passo@mongodb:27017/cucarachas";
+    const mongoDB = MONGODB_URI || "mongodb://localhost:27017/cucarachas";
+    console.log('url', mongoDB)
     mongoose
-      .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+      .connect(mongoDB)
       .then(() => console.log("connected to the db!!"))
       .catch((err) => {
         console.log("Cannot connect to the database!", err);
