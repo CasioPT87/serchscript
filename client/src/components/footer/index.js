d4f6f4050013ead180c732aa39f4e5c983554baf
Zerch <sergioibanezmoreno@gmail.com>
2022-12-10 09:18:43 +0100

    Footer

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -3,7 +3,11 @@ const { Link } = require('react-router-dom')
 const React = require('react')
 
 const Footer = () => {
-  return <div className="footer">this is the footer</div>
+  return (
+    <div className="footer">
+      <p className="footer__text">Memory is overrated</p>
+    </div>
+  )
 }
 
 module.exports = Footer
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,26 +1,26 @@
 module.exports = {
-    homeIcon: {
-        src: '/images/logo.png',
-        text: "serchscript blog logo"
+  homeIcon: {
+    src: '/images/logo.png',
+    text: 'serchscript blog logo',
+  },
+  home: {
+    path: '/',
+    text: 'Home',
+  },
+  articles: {
+    list: {
+      path: '/articles',
+      text: 'Articles',
     },
-    home: {
-        path: '/',
-        text: 'Home'
+    create: {
+      path: '/admin/articles/new',
+      text: 'New Article',
     },
-    articles: {
-        list: {
-            path: '/articles',
-            text: 'Articles'
-        },
-        create: {
-            path: "/admin/articles/new",
-            text: 'New Article'
-        }
+  },
+  auth: {
+    login: {
+      path: '/auth',
+      text: 'Login',
     },
-    auth: {
-        login: {
-            path: '/auth',
-            text: 'Login'
-        }
-    }
+  },
 }
