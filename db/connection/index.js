7ec477937b39b95f8b1502ee750e77ccee99cc30
Zerch <sergioibanezmoreno@gmail.com>
2023-03-22 17:34:00 +0100

    Dot not displaying at articles list

--- db/connection/index.js
+++ db/connection/index.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose')
 const { MONGODB_URI } = process.env
 
 const connect = () => {
+  console.log(
+    `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:27017/${process.env.DB_NAME}`
+  )
   const mongoDB =
     MONGODB_URI ||
-    `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:27017/${process.env.DB_USERNAME}`
+    `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:27017/${process.env.DB_NAME}`
   mongoose
     .connect(mongoDB)
     .then(() => console.log('connected to the db!!'))
